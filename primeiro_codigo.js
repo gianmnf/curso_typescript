667781ea1cb138d65319a6bf86e681daf4da0ccc
Gian Michel <gianmnf@unipam.edu.br>
2021-03-27 14:05:41 -0300

    Iniciando Desenvolvimento

---
+++ primeiro_codigo.js
@@ -0,0 +1,4 @@
+console.log("Hello World! Este é meu primeiro código TypeScript");
+console.log("==================");
+var nome = "Gian";
+console.log("Olá " + nome + " Seja bem-vindo!");
---
+++ primeiro_codigo.ts
@@ -0,0 +1,10 @@
+console.log("Hello World! Este é meu primeiro código TypeScript");
+
+console.log("==================");
+
+const nome = "Gian";
+
+console.log("Olá " + nome + " Seja bem-vindo!");
+
+console.log("==================");
+
